541a452d5dd5210f446fe4c995c20cfba926d90f
grebennikovartemii <grebennikovartemii27092013@gmail.com>
2026-08-01 15:14:41 +0300

    update backend

--- app/main.py
+++ app/main.py
@@ -97,6 +97,10 @@ def create_app() -> FastAPI:
         allow_origins=[
             "https://clever-dusk-364fcc.netlify.app",
             "https://cosmic-mandazi-cf5cc1.netlify.app",
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "http://localhost:4173",
+            "http://127.0.0.1:4173",
         ],
         allow_credentials=True,
         allow_methods=["*"],
